6d9b01f9f2bec1b0f0c901209c1ca6a141ff0e83
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-04-04 10:53:16 -0700

    make old uploader work with source/usercomment

--- main/views.py
+++ main/views.py
@@ -106,10 +106,14 @@ def iterate_files_upload(request):
     """
     files = FileMetaData.objects.all()
     for file in files:
+        vcf_source = request.POST.get('file_source_{}'.format(file.id))
+        user_notes = request.POST.get('file_desc_{}'.format(file.id))
         uploaded_file = request.FILES.get('file_{}'.format(file.id))
         if uploaded_file is not None:
             metadata = {'tags': json.loads(file.tags),
-                        'description': file.description}
+                        'description': file.description,
+                        'user_notes': user_notes,
+                        'vcf_source': vcf_source}
             upload_file_to_oh(
                 request.user.openhumansmember,
                 uploaded_file,
